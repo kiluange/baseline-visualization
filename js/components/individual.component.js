67eac5e8b507c53577d1b8826cb0f88233f90255
Daniel Kiluange <daniel.kiluange@gmail.com>
2018-12-14 17:24:17 -0300

    recriando projeto

---
+++ js/components/individual.component.js
@@ -0,0 +1,8 @@
+export const Home = Vue.component('home', {
+    template: `<div class="jumbotron">
+    <h1 class="display-4">mACS</h1>
+    <p class="lead">O mACS é um projeto de pesquisa para incorporar tecnologias móveis (mHealth) a rotina de trabalho da Estratégia de Saúde da Família do SUS. Ao fazê-lo, o mACS pretende possibilitar a análise inteligente e automática dos dados (big data) para gerar informações relevantes acerca da saúde pública no Brasil.</p>
+    <hr class="my-4">
+    <p>Leia nosso Wiki se deseja contribuir com o desenvolvimento do projeto ou se deseja construir uma solução própria baseada no projeto mACS.</p>
+  </div>`,
+});
